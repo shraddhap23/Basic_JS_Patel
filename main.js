d190da47829c7b78a8b095e27d9a7a730036d9f6
Shraddha Patel <spatelrb@gmail.com>
2016-02-15 12:12:33 -0500

    basic java cheat sheet

---
+++ main.js
@@ -0,0 +1,48 @@
+//Problem 1
+var x = 11;
+var y = 3;
+console.log (x + y);
+
+var who = "Donald Trump"
+var what = " is a muppet"
+console.log (who + what);
+
+//Problem 2 
+var vodka = ["42 Below", "Titos"];
+var gin = ["Hendricks", "Bombay Sapphire"];
+var alcohol = [vodka, gin];
+var both = [ [ "42 Below", "Titos"], ["Hendricks", "Bombay Sapphire"] ];
+console.log(alcohol[0][1]);
+console.log(alcohol[1][0]);
+
+//Problem 3
+var z = 7;
+if (z<10){
+	alert ("Variable is less than 10");
+} else{
+	console.log ("This won't show up because z is less than 10");
+}
+
+//Problem 4
+var w = 18;
+if (w<10){
+	alert ("Variable is less than 10");
+} else{
+	console.log ("This will show up because w is more than 10");
+}
+
+//Problem 6
+function alertName (Name){
+	return alert (Name);
+}
+alertName("Shraddha");
+
+//Additional Problem 
+//Declare a function that depending upon which virtual 'door' was entered 
+//tells the user they've received a different 'prize' in an alert. After declaring 
+//the function, try running it with different options.
+
+
+
+
+
